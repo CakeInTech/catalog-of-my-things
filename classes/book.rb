3c84a6f7a77a63f5b152a4ae21638c54979fb398
MohamedCK <abdulhakimm280@gmail.com>
2023-01-16 17:01:48 +0200

    fix linter offeces

--- classes/book.rb
+++ classes/book.rb
@@ -1,17 +1,17 @@
 require_relative './item'
 
-class Book < Item 
-    attr_accessor :publisher, :cover_state, :label
+class Book < Item
+  attr_accessor :publisher, :cover_state, :label
 
-    def initialize(publisher, cover_state, *arg)
-        super(*args)
-        @publisher = publisher
-        @cover_state = cover_state
-    end 
+  def initialize(publisher, cover_state, *arg)
+    super(*args)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    # private 
+  # private
 
-    # def can_be_archived? 
-    #     super or @cover_state == 'bad'
-    # end
+  # def can_be_archived?
+  #     super or @cover_state == 'bad'
+  # end
 end
