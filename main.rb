c69cee91e3ca9db0cf77ce674205293f7a844a03
MohamedCK <abdulhakimm280@gmail.com>
2023-01-17 11:38:32 +0200

    - display options when the app starts

--- main.rb
+++ main.rb
@@ -12,6 +12,21 @@ class Main
             new_app.extra_option(option) if option.between?(7, 9) 
         end 
     end
+    
+    def select_option
+        puts 'Welcome to the catalog app'
+        puts 'Please choose an option by entering a number:'
+        puts '1  - List all books'
+        puts '2  - List all music albums'
+        puts '3  - List all games'
+        puts '4  - List all genres'
+        puts '5  - List all labels'
+        puts '6  - List all authors'
+        puts '7  - Add a book'
+        puts '8  - Add a music album'
+        puts '9  - Add a game'
+        puts '10 - exit'
+    end
 end
 
 def main 
