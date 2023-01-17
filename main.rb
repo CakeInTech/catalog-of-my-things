a3ccb5d282feb72ab3d7952d2846e188ac86d8a3
MohamedCK <abdulhakimm280@gmail.com>
2023-01-17 11:37:36 +0200

    - Create a `Start` for main.rb file

--- main.rb
+++ main.rb
@@ -1,2 +1,21 @@
-def main
-main()
+require_relative './app'
+
+class Main 
+    def start 
+        new_app = App.new
+        loop do 
+            select_option
+            option = gets.chomp.to_i
+            break if option == 10
+
+            new_app.select_option(option)
+            new_app.extra_option(option) if option.between?(7, 9) 
+        end 
+    end
+end
+
+def main 
+    Main.new.start
+end
+
+main
