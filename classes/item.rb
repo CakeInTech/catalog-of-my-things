90f10cafa3f4942119898241a606cc84e9bb185f
MohamedCK <abdulhakimm280@gmail.com>
2023-01-18 15:58:37 +0200

    - Fix failed linters

--- classes/item.rb
+++ classes/item.rb
@@ -1,11 +1,10 @@
 require 'date'
 
 class Item
-
   attr_accessor :publish_date
-  attr_reader :genre, :id, :archived,  :author, :label
+  attr_reader :genre, :id, :archived, :author, :label
 
-  def initialize(genre, author, source, label, publish_date)
+  def initialize(_genre, _author, _source, _label, publish_date)
     @id = Random.rand(1..1000)
     @publish_date = publish_date
     @archived = true
@@ -13,11 +12,11 @@ class Item
 
   def can_be_archived?
     published_year = Date.strptime(@published_date, '%d-%m-%Y').strftime('%Y')
-    Time.now.year - published_year.to_i > 10 
+    Time.now.year - published_year.to_i > 10
   end
 
   def add_author(author)
-    author.extra_option << self unless author.items.include? self 
+    author.extra_option << self unless author.items.include? self
     @author = author
   end
 end
