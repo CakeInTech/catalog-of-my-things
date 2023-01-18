488b6b727fce2098ef7493012cee4e09e5e73e0a
Suleiman Olaide <olaide.suleiman96@gmail.com>
2023-01-18 14:53:57 -0800

    Fix attribute name

--- classes/author.rb
+++ classes/author.rb
@@ -1,5 +1,5 @@
 class Author
-  attr_reader :id, :first_namek, :last_name, :items
+  attr_reader :id, :first_name, :last_name, :items
 
   def initialize(first_name, last_name)
     @id = Random.rand(1..1000)
