3c7f8382fe37bf201d797a9782b6cac79fb56e53
MohamedCK <abdulhakimm280@gmail.com>
2023-01-20 11:18:25 +0200

    - Fix linter errors

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require './classes/game'
 
 describe Game do
   before :each do
-    @game = Game.new( 'YES', '2001-09-11', '1997-02-06')
+    @game = Game.new('YES', '2001-09-11', '1997-02-06')
   end
   it 'returns the correct last played date' do
     expect(@game.last_played).to eq '2001-09-11'
