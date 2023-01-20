69923f9f8035005ce2bc6230baeb40116c917cf8
Suleiman Olaide <olaide.suleiman96@gmail.com>
2023-01-20 07:38:27 -0800

    Add unit tests for game and authir classes

---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+require './classes/game'
+
+describe Game do
+  before :each do
+    @game = Game.new( 'YES', '2001-09-11', '1997-02-06')
+  end
+  it 'returns the correct last played date' do
+    expect(@game.last_played).to eq '2001-09-11'
+  end
+  it 'returns the correct multiplayer status' do
+    expect(@game.multiplayer).to eq 'YES'
+  end
+  it 'returns the correct publish date' do
+    expect(@game.publish_date).to eq '1997-02-06'
+  end
+end
